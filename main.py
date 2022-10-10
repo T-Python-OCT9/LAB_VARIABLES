1e1d1c95d279494780240f47692b9cbe4505fbbc
moath alosaimi <moathalosimie@hotmail.com>
2022-10-10 20:02:40 +0300

    Lab1

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+
+my_favorite_team  : str = "manchester city"
+print(my_favorite_team)
+# Define a variable that has a value of your favourite football team.
+x : int = 5
+y  : float = 3
+print(x,y)
+#- Define a variable “x“  has an integer value and a variable “y” has a float value.
+
+val1 = val2 = 25
+print(val1,val2)
+ #Assign One Value to Multiple Variables (choose random names & values).
+
+v1, v2 = 10 , 25
+print(v1,v2) 
+#- Assign multiple Values to Multiple Variables (choose random names & values).
