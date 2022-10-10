c3b99ea522714f4a3ac2e865f7c8dfc6ba1b77d9
Faris A <Faris1d.sa@gmail.com>
2022-10-10 19:58:36 +0300

    I sloved lab1

---
+++ lab1.py
@@ -0,0 +1,14 @@
+my_Fav_FootballTeam = 'ALHILAL'
+
+x = 12
+
+y = 12.45
+
+var1 = var2 = var3 = 24
+
+A, B, C = 1, 2, 5
+
+print(my_Fav_FootballTeam)
+print(x,"and", y)
+print(var2)
+print(A,",", B,",", C)
