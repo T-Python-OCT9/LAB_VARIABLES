ba7deea393e661b2a5ea0fe383a8d723d973afe5
bahiyyah_4 <bahiab144@gmail.com>
2022-10-10 20:29:23 +0300

    add lab variables

--- lab1.py
+++ lab1.py
@@ -2,7 +2,7 @@ my_favourite_team: str = "nothing"
 x: int = 8
 y: float = 4.44
 team1 = team2 = team3 = 33
-team1_score, team2_score = 44, 30
+team1_score, team2_score = 44, "NAN"
 print(my_favourite_team)
 print(x)
 print(y)
