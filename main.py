963ce00a478df4dc9614739e5599284f510af2c9
Abdulmohsen Alateeq <mohsenalateeq@gmail.com>
2022-10-10 20:02:01 +0300

    Add favorite team informations

---
+++ main.py
@@ -0,0 +1,14 @@
+# - Define a variable that has a value of your favourite football team.
+favorite_football_team: str = "Manchester United"
+print(favorite_football_team)
+# - Define a variable “x“  has an integer value and a variable “y” has a float value.
+x: int = 21
+y: float = 21.03
+
+# - Assign One Value to Multiple Variables (choose random names & values).
+varane_cl = casemiro_cl = ronaldo_cl = "5 Champions League"
+print(casemiro_cl)
+
+# - Assign multiple Values to Multiple Variables (choose random names & values).
+varane, casemiro, ronaldo = 19, 18, 7
+print(varane, casemiro, ronaldo)
