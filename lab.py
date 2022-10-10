f4e48184cb3924e8e9711cba01aa5a9af4fc30b4
rawan waleed <rawanw.r99@gmail.com>
2022-10-10 19:58:40 +0300

    Fist python lab

---
+++ lab.py
@@ -0,0 +1,14 @@
+fav_football_team :str = "Hilal"
+print(fav_football_team)
+
+x : int  = 20 
+y : float = 10.5
+print (x , y)
+
+
+v1 = v2  = v3 = "First Python lab"
+print(v1)
+print(v2)
+
+var1,var2,var3 = "rawan" , " waleed " , 24
+print(var1,var2,var3)
