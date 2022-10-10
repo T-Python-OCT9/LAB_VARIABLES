938e8a9cd859c48cb61eafc6507cb47eca32d388
Asrar alrashed <asraralrashed2@gmail.com>
2022-10-10 20:20:06 +0300

    done the first LAB

---
+++ lap1.py
@@ -0,0 +1,18 @@
+#Define a variable that has a value of your favourite football team. ?
+var_football_team : str ="madrid"
+print(var_football_team)
+
+#Define a variable “x“  has an integer value and a variable “y” has a float value.
+var1 , var2 = 20 ,20.5
+x :int=(50)
+y :float=(50.3) 
+print(var1,var2)
+print (x,y)
+
+#Assign One Value to Multiple Variables (choose random names & values).
+var3 = var4 = var5 = 39
+print(var4) 
+
+#Assign multiple Values to Multiple Variables (choose random names & values).
+name , age= "asrar", 23
+print(name,age)
