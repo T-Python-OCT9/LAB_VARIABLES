e98253f090e4559af2c41c7d3e8aba2f0a3b13a4
Najd <nani1997.na@gmail.com>
2022-10-10 20:40:50 +0300

    solves the first lab

---
+++ main.py
@@ -0,0 +1,18 @@
+my_fav_team :str = "Liverpool"
+print(my_fav_team)
+
+
+x : int = 12
+y : float= 7.10
+print('x',x ,'y',y)
+
+var1=var2=var3 =11
+
+print(var1)
+
+var4, var5= 25, "Hello There"
+
+print(var4, var5)
+
+
+
