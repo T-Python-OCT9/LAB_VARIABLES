301d13cc36f4aa1708a0bde95cd4188cd33f77a6
Alaa Alfraih <Alfraih.alaa@gmail.com>
2022-10-10 20:23:45 +0300

    lap python

---
+++ main.py
@@ -0,0 +1,15 @@
+favoritTeam="I do not like any team"
+x:int=2
+y:float=4.1
+val1 = val2 = val3 =20
+name1, name2 , ="Alaa","ALfraih"
+
+#print to test 
+print(favoritTeam)
+print(x)
+print(y)
+print(val1)
+print(val2)
+print(val3)
+print(name1)
+print(name2)
