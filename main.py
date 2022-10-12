a7875d337677e014f081757f6d2afeee8b99f827
Razan.abdullah <rrazanabdullahh@gmail.com>
2022-10-12 19:49:10 +0300

    SolvedFirstV

---
+++ main.py
@@ -0,0 +1,17 @@
+'''
+FIRST LAB (PYTHON) 
+ '''
+print("Define a variable that has a value of your favourite football team ")
+print(" Define a variable “x“ has an integer value and a variable “y” has a float value ----")
+x : int = 10
+y : float = 20
+print("x : " , x , " y : ", y)
+
+print("Assign One Value to Multiple Variables (choose random names & values) ")
+Razan = Raghad = Ghadah = " Team "
+print("Print all values to test them : " , Razan , " | " , Raghad , " | " , Ghadah)
+print("Assign multiple Values to Multiple Variables (choose random names & values) ")
+Rz , Rg , Gh = "A" , 1 , 2
+print("First Value : ", Rz)
+print("Second Value : ", Rg)
+print("Third Value : ", Gh)
